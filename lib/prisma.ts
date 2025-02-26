963cdfd1a81e11bdbf211e143febf0a577bdcbc7
sameercr7 <sameer05072001@gmail.com>
2025-02-26 20:37:54 +0530

    prisma fix trial3.0

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,32 @@
-// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+// // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+
+// declare global {
+//   var prisma: PrismaClient;
+// }
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
 
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
+
+// Ensure Prisma is initialized only once
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
