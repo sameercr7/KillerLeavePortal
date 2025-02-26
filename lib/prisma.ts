79b4a7ae71160980ba93d6539c9cbdb0e9697a1e
sameercr7 <sameer05072001@gmail.com>
2025-02-26 22:59:39 +0530

    prisma fix trial_10.0

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -21,11 +21,20 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+// Ensure Prisma is initialized only once
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
